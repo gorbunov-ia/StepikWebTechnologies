8568dd3eeb600f1c94a70f311a3d7d3ba579977a
Ivan Gorbunov <Grbnv.vn@gmail.com>
2017-05-23 23:01:03 +0300

    Add setting nginx

--- init.sh
+++ init.sh
@@ -1,2 +1,3 @@
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+#sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/conf.d/test.conf
 sudo /etc/init.d/nginx restart
